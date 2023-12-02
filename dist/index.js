7c277e41adb6d2e49c3a0fb3f017f33c5fd2198a
lexandr10 <alexandrenko3006@gmail.com>
2023-12-02 10:32:51 -0500

    3

--- dist/index.js
+++ dist/index.js
@@ -2,8 +2,8 @@ import { concatenation } from "./concatenation";
 const button = document.querySelector("button");
 const input = document.querySelector("input");
 if (button && input) {
-  button.addEventListener("click", () => {
-    concatenation(input.value, "hello!");
-  });
+    button.addEventListener("click", () => {
+        concatenation(input.value, "hello!");
+    });
 }
-// sourceMappingURL = index.js.map;
+//# sourceMappingURL=index.js.map
